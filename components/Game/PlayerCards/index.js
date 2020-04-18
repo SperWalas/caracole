df3e4951578d01d1b44d2a286ad7e00ca6f30f00
Benoit de La Forest <benoit.delaforest@margo.com>
2020-04-18 18:12:25 +0200

    Add halo behind current player

--- components/Game/PlayerCards/index.js
+++ components/Game/PlayerCards/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
+import useCardActions from '../../../hooks/useCardActions';
 import useCardSpots, { getPlayerCardSpotId } from '../../../hooks/useCardSpots';
 import CardSpot from '../../CardSpot';
 import { StyledColumn } from './_styled';
 
 const PlayerCards = ({ player, position }) => {
   const { cards, id: playerId } = player;
+  const { nextAction, nextPlayer } = useCardActions();
   const { setCardSpotRef } = useCardSpots();
   const spots = Array.apply(null, Array(Math.max(cards.length, 4)));
+  const isPlayerToPlay = nextPlayer && nextAction && playerId === nextPlayer.id;
 
   return (
-    <StyledColumn cardCount={spots.length} position={position}>
+    <StyledColumn cardCount={spots.length} isHighlighted={isPlayerToPlay} position={position}>
       {spots.map((_, idx) => (
         <CardSpot
           key={`${playerId}${idx}`}
