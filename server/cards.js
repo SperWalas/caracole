659705af812207a9be0a7e96bd016e5865bc47e7
Samuel Renault <sams.renault@gmail.com>
2020-04-22 21:16:19 +0200

    Remove debug deck

--- server/cards.js
+++ server/cards.js
@@ -2,8 +2,7 @@ const uuidv4 = require('uuid').v4;
 
 // Constants
 const suits = ['spades', 'diamonds', 'clubs', 'hearts'];
-// const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-const values = ['A', '2']; //, '3'], '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
 /**
  * Data format
